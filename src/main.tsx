738289bbef9c2f066d44f0c37b0082e09388ebe0
Civic Education Kenya <civiceducationkenya@gmail.com>
2025-04-29 23:36:29 +0300

    Update main.tsx
    Typescript error correction in BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { Browser Router } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById("root")!).render(
   <Browser>
