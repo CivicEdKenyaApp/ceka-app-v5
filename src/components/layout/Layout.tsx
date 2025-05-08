5e75b4ed9cd413d0f9e3e2d8fe9b5f2a753f03d8
Civic Education Kenya <civiceducationkenya@gmail.com>
2025-05-08 21:22:49 +0300

    Update Layout.tsx
    Last NavBar edit

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -16,16 +16,18 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <LanguageProvider>
       <ThemeProvider>
-        <div className="flex min-h-screen flex-col">
+        <div className="flex min-h-screen flex-col relative">
           <ScrollListener>
             <div className="flex flex-col flex-1">
               <Navbar />
               <main className="flex-1 pb-16 md:pb-0">{children}</main>
               <Footer />
-              <BottomNavbar />
               <BackButton />
             </div>
           </ScrollListener>
+
+          {/* Move BottomNavbar here, outside scrollable layout */}
+          <BottomNavbar />
         </div>
       </ThemeProvider>
     </LanguageProvider>
