4d96ff00a25275182ae93d303c78db4a750fc673
Civic Education Kenya <civiceducationkenya@gmail.com>
2025-05-09 06:54:20 +0300

    Update App.tsx
    Added the Scroll to top functionality which had somewhat been removed from code, since it was there. Hope it works

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,8 @@ import UserProfile from './pages/UserProfile';
 import FeedbackPage from './pages/FeedbackPage';
 import NotFound from './pages/NotFound';
 import PullToRefresh from '@/components/ui/PullToRefresh';
+import ScrollToTop from './components/ScrollToTop.tsx'; 
+
 
 // Add these imports for settings pages:
 import SettingsLayout from "./pages/settings/SettingsLayout";
@@ -86,6 +88,7 @@ const App = () => {
           <AuthContext.Provider value={{ session, signOut }}>
             <Toaster />
             <PullToRefresh>
+              <ScrollToTop />
               <Routes>
                 <Route path="/" element={<Index />} />
                 <Route path="/auth" element={<AuthPage />} />
